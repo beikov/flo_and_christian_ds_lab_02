b5d0127f6bce8615b56008bda76616a44033a9e3
Florian Zaruba <fl.zaruba@be4web.net>
2012-11-23 17:04:06 +0100

    RMI

--- src/ds02/server/event/handler/BidOverBidHandler.java
+++ src/ds02/server/event/handler/BidOverBidHandler.java
@@ -7,20 +7,7 @@ import ds02.server.model.Auction;
 import ds02.server.service.BidService;
 import ds02.server.service.UserService;
 
-public class BidOverBidHandler implements EventHandler<BidOverbidEvent> {
-    
-    @Override
-    public void handle(BidOverbidEvent event) {
-    	final Auction auction = BidService.INSTANCE.getAuction(event.getAuctionId());
-        final UserConnection userConnection = UserService.INSTANCE.getUser(event.getUsername());
-        
-        /* Not specified if a user should be notified if he overbid himself, so we notify him */
-        if(userConnection != null){
-            final StringBuilder sb = new StringBuilder();
-            sb.append("!new-bid ").append(auction.getDescription());
-            
-//            userConnection.asyncResponse(sb.toString());
-        }
-    }
+public class BidOverBidHandler extends AbstractEventHandler<BidOverbidEvent> {
+
     
 }
